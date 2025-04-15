bc09d80643a912abe42bfe23a8087d1ea5ab1767
Dharanisree2004 <118334120+Dharanisree2004@users.noreply.github.com>
2025-04-15 09:22:44 +0530

    Add files via upload

---
+++ url-shortener-master/src/main/java/dev/url/UrlShortener/Links/LinkRepository.java
@@ -0,0 +1,9 @@
+package dev.url.UrlShortener.Links;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LinkRepository extends JpaRepository<Link, Long> {
+    Link findByShortCode(String shortCode);
+}
+
+
